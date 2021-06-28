6b60e7397d1b2e4bb8fd065f2e318a3f4c1aa17d
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-28 23:18:39 +0530

    add auth routes

---
+++ src/routes/authRouter.ts
@@ -0,0 +1,17 @@
+
+import { Router } from 'express'
+import AuthCtrl from '../controllers/AuthControl'
+
+const router = Router()
+const authCtrl = new AuthCtrl()
+
+router.post('/register', authCtrl.register)
+
+router.post('/login', authCtrl.login)
+
+router.post('/logout', authCtrl.logout)
+
+router.post('/refresh_token', authCtrl.generateAccessToken)
+
+
+export default router
